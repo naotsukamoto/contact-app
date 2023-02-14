01bae4eb8c1260d646253eb6fe6646938c12c624
塚本尚 <tsukamotonao@tsukamotonaos-MacBook-Air.local>
2023-02-15 00:09:01 +0900

    edit InputDate input

--- src/components/atoms/InputDate.tsx
+++ src/components/atoms/InputDate.tsx
@@ -1,12 +1,21 @@
+import { format, isToday } from "date-fns";
+import ja from "date-fns/locale/ja";
+
 type Props = {
   dt: Date;
 };
 
 export const InputDate: React.FC<Props> = (props) => {
   const { dt } = props;
+  const today = new Date();
   return (
     <>
-      <input type="date" min="2023-01-01" max="2023-03-31"></input>
+      <input
+        type="date"
+        min={format(today, "yyyy-MM-dd", { locale: ja })}
+        max="2023-03-31"
+        value={format(dt, "yyyy-MM-dd", { locale: ja })}
+      ></input>
     </>
   );
 };
