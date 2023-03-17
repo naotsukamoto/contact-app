73924ae54a57f50d30c60bc8eae33c1f1f52e65b
塚本尚 <tsukamotonao@tsukamotonaos-MacBook-Air.local>
2023-03-18 01:13:46 +0900

    refactoring

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -26,14 +26,16 @@ const transport = {
 // 送信に使用するメールサーバーの設定
 const transporter = nodemailer.createTransport(transport);
 
-// メール送信成功フラグと変数
-// let mailCount = 0;
-const today = new Date();
-
 exports.sendMail = functions
   .region("asia-northeast1")
-  .pubsub.schedule("every 1 days")
+  .pubsub.schedule("0 20 * * *")
+  .timeZone("Asia/Tokyo")
   .onRun(async (context) => {
+    // メール送信成功フラグと変数
+    let mailCount: number = 0;
+    let times: number = 0;
+    const today: Date = new Date();
+
     // まず、Firestore からデータを持ってくる
     const snapshot = await app.firestore().collection("users").get();
     // それぞれに対してメールを送る
@@ -73,22 +75,27 @@ exports.sendMail = functions
             transporter.sendMail(mailOptions, (err, res) => {
               console.log(err || res);
             });
+            // 送信件数をカウント
             mailCount += 1;
           }
         }
+
+        // ループ数をカウント
+        times += 1;
+        if (times === 6) {
+          console.log("メール送信します");
+          // 毎日運営にもメールを送って状況を確認する
+          const mailOptions = {
+            from: "no-replay@conconcontacts.com",
+            to: "m0naaa0u@gmail.com",
+            subject: "運営向け：定期メール送信について | ConCon",
+            text: `${today}分の定期メール送信${mailCount}件です。`,
+          };
+          transporter.sendMail(mailOptions, (err, res) => {
+            console.log(err || res);
+          });
+        }
       });
     });
-
-    // 毎日運営にもメールを送って状況を確認する
-    const mailOptions = {
-      from: "no-replay@conconcontacts.com",
-      to: "m0naaa0u@gmail.com",
-      subject: "運営向け：定期メール送信について | ConCon",
-      text: `${today}分の定期メール送信xx件です。`,
-    };
-    transporter.sendMail(mailOptions, (err, res) => {
-      console.log(err || res);
-    });
-
     return null;
   });
